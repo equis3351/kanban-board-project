14bb458e07d20bbfb8eef397d32c43834b59d387
equis3351 <equis3351@naver.com>
2024-07-09 17:26:42 +0900

    init

---
+++ src/main/java/com/sparta/kanbanboardproject/KanbanBoardProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.kanbanboardproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanBoardProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanbanBoardProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/kanbanboardproject/KanbanBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.kanbanboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
