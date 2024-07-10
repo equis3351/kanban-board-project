d0109bcc7466b6e7015eb848d23ae7c5947453dd
leejinuk <leejinouk123@kakao.com>
2024-07-10 19:53:20 +0900

    update: user login, logout, signup feature 추가

--- src/main/java/com/sparta/kanbanboardproject/domain/user/entity/User.java
+++ src/main/java/com/sparta/kanbanboardproject/domain/user/entity/User.java
@@ -1,4 +1,43 @@
 package com.sparta.kanbanboardproject.domain.user.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "users")
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserRoleEnum role;
+
+    @Setter
+    @Column(name = "refresh_token")
+    private String refreshToken;
+
+    public User(String username, String password, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public void logoutUser() {
+        refreshToken = "logged out";
+    }
 }
