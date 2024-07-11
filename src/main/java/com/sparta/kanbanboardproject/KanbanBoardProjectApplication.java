c3e16468658239cb4005b34559b1150c817237a8
K <xv40101392@gmail.com>
2024-07-11 13:35:07 +0900

    ♻️ refactor: 코드 컨벤션 개선

--- src/main/java/com/sparta/kanbanboardproject/KanbanBoardProjectApplication.java
+++ src/main/java/com/sparta/kanbanboardproject/KanbanBoardProjectApplication.java
@@ -3,9 +3,7 @@ package com.sparta.kanbanboardproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing
 @SpringBootApplication
 //        (exclude =  SecurityAutoConfiguration.class )//시큐리티 제외
 public class KanbanBoardProjectApplication {
