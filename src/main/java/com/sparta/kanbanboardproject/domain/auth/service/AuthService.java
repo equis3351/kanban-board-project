732dfaeffe1d02889baff5eed7097db03b7f6582
leejinuk <leejinouk123@kakao.com>
2024-07-12 11:55:00 +0900

    refactor: Add NOT_FOUND_EXCEPTION handling for user and token

--- src/main/java/com/sparta/kanbanboardproject/domain/auth/service/AuthService.java
+++ src/main/java/com/sparta/kanbanboardproject/domain/auth/service/AuthService.java
@@ -2,6 +2,8 @@ package com.sparta.kanbanboardproject.domain.auth.service;
 
 import com.sparta.kanbanboardproject.domain.user.entity.User;
 import com.sparta.kanbanboardproject.domain.user.repository.UserRepository;
+import com.sparta.kanbanboardproject.global.exception.CustomException;
+import com.sparta.kanbanboardproject.global.exception.ErrorType;
 import com.sparta.kanbanboardproject.global.jwt.JwtService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -25,11 +27,11 @@ public class AuthService {
         String username = jwtService.extractUsername(refreshToken);
 
         User getUser = userRepository.findByUsername(username).orElseThrow(
-                () -> new IllegalArgumentException("유저를 찾을 수 없습니다.")
+                () -> new CustomException(ErrorType.NOT_FOUND_USER)
         );
 
         if (!refreshToken.equals(getUser.getRefreshToken().substring(7))) {
-            throw new IllegalArgumentException("refreshToken이 유효하지 않습니다.");
+            throw new CustomException(ErrorType.NOT_FOUND_TOKEN);
         }
 
         // refreshToken 유효성 확인
@@ -42,7 +44,9 @@ public class AuthService {
             String newRefreshToken = jwtService.createRefreshToken(username);
             log.info("새로운 refresh token : {}", newRefreshToken);
 
-            User saveUser = userRepository.findByUsername(username).orElseThrow(IllegalArgumentException::new);
+            User saveUser = userRepository.findByUsername(username).orElseThrow(
+                    ()->new CustomException(ErrorType.NOT_FOUND_USER)
+            );
             saveUser.setRefreshToken(newRefreshToken);
             userRepository.save(saveUser);
 
--- src/main/java/com/sparta/kanbanboardproject/global/exception/ErrorType.java
+++ src/main/java/com/sparta/kanbanboardproject/global/exception/ErrorType.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum ErrorType {
     // LOGIN
     INVALID_ACCOUNT_ID(HttpStatus.UNAUTHORIZED, "아이디가 일치하지 않습니다."),
-    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다.");
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
+
+    NOT_FOUND_TOKEN(HttpStatus.NOT_FOUND, "토큰을 찾을 수 없습니다."),
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
