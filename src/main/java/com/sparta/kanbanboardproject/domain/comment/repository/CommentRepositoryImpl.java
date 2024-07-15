0473420caa72fe96a77c560e33b7c038c7e8bacd
K <xv40101392@gmail.com>
2024-07-15 16:36:07 +0900

    ✨ update: Progress, Comment, Collaborator Query DSL .join().fetchJoin 수정

--- src/main/java/com/sparta/kanbanboardproject/domain/comment/repository/CommentRepositoryImpl.java
+++ src/main/java/com/sparta/kanbanboardproject/domain/comment/repository/CommentRepositoryImpl.java
@@ -2,14 +2,14 @@ package com.sparta.kanbanboardproject.domain.comment.repository;
 
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.kanbanboardproject.domain.card.entity.QCard;
 import com.sparta.kanbanboardproject.domain.comment.entity.Comment;
+import com.sparta.kanbanboardproject.domain.comment.entity.QComment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
-import static com.sparta.kanbanboardproject.domain.comment.entity.QComment.comment;
 
 
 @Repository
@@ -20,11 +20,14 @@ public class CommentRepositoryImpl implements CustomCommentRepository {
 
 
     public List<Comment> findAllByCardId(Long cardId, Sort createdAt){
+        QComment comment=QComment.comment;
+        QCard card=QCard.card;
+
         return jpaQueryFactory
                 .select(comment)
                 .from(comment)
+                .join(comment.card, card).fetchJoin()
                 .where(comment.card.id.eq(cardId))
-                .fetchJoin()
                 .fetch();
     }
 }
