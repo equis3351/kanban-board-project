b2deb997cb9caa8f4db86a5da95839fe8525d59d
K <xv40101392@gmail.com>
2024-07-15 15:46:35 +0900

    ✨ update: Progress, User, Collaborator Query DSL 수정
    User Query DSL 삭제

--- src/main/java/com/sparta/kanbanboardproject/domain/user/repository/CollaboratorRepositoryQuery.java
+++
@@ -1,5 +0,0 @@
-package com.sparta.kanbanboardproject.domain.user.repository;
-
-public interface CollaboratorRepositoryQuery {
-    boolean UserCheck(Long invitedUserId);
-}
--- src/main/java/com/sparta/kanbanboardproject/domain/user/repository/UserRepositoryQuery.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.kanbanboardproject.domain.user.repository;
-
-import com.sparta.kanbanboardproject.domain.user.entity.User;
-
-import java.util.Optional;
-
-public interface UserRepositoryQuery {
-    Optional<User> searchUser(String username);
-}
